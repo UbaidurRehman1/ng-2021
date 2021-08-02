7e0564a5eadb8962bf46b39c6ee980f24226a592
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-08-02 11:32:33 +0500

    - components data binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    @Output('onSelectFeature') onSelectFeatureEmitter: EventEmitter<string> = new EventEmitter<string>();
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+    }
+
+    onSelectFeature(feature: string) {
+        this.onSelectFeatureEmitter.emit(feature);
+    }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,11 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
-    recipes: Recipe[] = [new Recipe('Recipe1', 'A good one', 'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg')];
+    recipes: Recipe[] = [
+        new Recipe('Recipe1', 'A good one', 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=220&q=10'),
+        new Recipe('Recipe2', 'A good two', 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=220&q=10'),
+        new Recipe('Recipe2', 'A good three', 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=220&q=10')
+    ];
 
     constructor() {
     }
