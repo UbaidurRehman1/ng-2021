7bf0d416c0237a32c64441e05b0ba17b2d327328
UbaidurRehman1 <ubaidshan007@gmail.com>
2022-06-20 18:36:59 +0500

    add ng-demo project

---
+++ ng-demo/src/app/search/search.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {Person, SearchService} from '../shared';
+
+@Component({
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+    query: string | undefined;
+    searchResults: Person[] = [];
+
+    constructor(private searchService: SearchService) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    search(): void {
+        this
+            .searchService
+            .getAll()
+            .subscribe({
+                next:(data: Person[]) => {this.searchResults = data},
+                error: (e: any) => console.log(e)
+            })
+    }
+
+}
